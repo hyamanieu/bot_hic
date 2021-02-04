150f6569c4371938d85064e4e283ad6b00ff9148
Yannick JOST <yannick.jost@alsacedigitale.org>
2021-02-04 19:13:46 +0100

    configure welcome mode in settings

--- extensions/settings.py
+++ extensions/settings.py
@@ -1,4 +1,6 @@
+import os
+
 class Settings:
     def __init__(self):
-        self.ADMIN_ROLE='Support'
-        self.WELCOME_MODE='close'  #mode 'open' ou 'close'
+        self.ADMIN_ROLE=os.getenv('BOT_ADMIN_ROLE','Support')
+        self.WELCOME_MODE=os.getenv('BOT_WELCOME_MODE', 'close')  #mode 'open' ou 'close'
